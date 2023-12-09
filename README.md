46b0530904dc7bf60f6a88bbdd5e33272e9c023b
riordanmb <riordanmb@gmail.com>
2023-12-09 18:38:00 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+PDF Custom Metadata Demonstration
+============================
+
+This is a demonstration of how to use custom metadata in a PDF file.
+
+
+* [dummy.pdf](dummy.pdf) is a PDF file with no custom metadata.
+* [sample_dummy_with_data.pdf](sample_dummy_with_data.pdf) is a PDF file containing several random metadata strings.
+* [generate_random_metadata.py](generate_random_metadata.py) will produce a new file from the dummy file, called _dummy\_with\_data\.pdf_, which adds several randomly-generated items of metadata.
+
+There are various tools for interacting with PDF metadata.
+
+Many tools, including Apple's Preview and DEVONthink, do not allow easy access to custom metadata. But you can view it using Adobe Acrobat. Select File->Document Properties->Custom.
+
+For getting and setting custom metadata, I have used the PDFReader and PDFWriter classes of the [PyPDF library](https://pypi.org/project/PyPDF2/1.23/). 
+
+You can also use [ExifTool](https://github.com/exiftool/exiftool) by Phil Harvey, which is available as either a Perl module, or a standalone commandline executable. 
---
+++ generate_random_metadata.py
@@ -0,0 +1,72 @@
+import random
+from datetime import datetime, timedelta
+from PyPDF2 import PdfReader, PdfWriter
+
+
+# Function to generate a random date
+def generate_random_date():
+    start_date = datetime(1900, 1, 1)  # You can adjust the start date as per your needs
+    end_date = datetime(2100, 12, 31)  # You can adjust the end date as per your needs
+
+    random_date = start_date + timedelta(days=random.randint(0, (end_date - start_date).days))
+
+    return random_date
+
+
+# Function to generate random custom metadata
+def generate_random_metadata():
+    metadata = {}
+
+    # Generate 5 random keys and values in various formats
+    for _ in range(5):
+        key = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(5))  # Random key
+        value_type = random.choice(["string", "integer", "float", "date"])  # Random value type
+
+        if value_type == "string":
+            value = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') for _ in
+                            range(10))  # Random string
+        elif value_type == "integer":
+            value = random.randint(1, 100)  # Random integer
+        elif value_type == "float":
+            value = round(random.uniform(1.0, 100.0), 2)  # Random float with 2 decimal places
+        elif value_type == "date":
+            value = generate_random_date()  # Random date
+
+        metadata['/' + key] = value
+
+    return metadata
+
+
+# Function to set custom metadata in a PDF document
+# Function to set custom metadata in a PDF document
+def set_pdf_metadata(filepath, organization, custom_metadata):
+    with open(filepath, 'rb') as file:
+        reader = PdfReader(file)
+        writer = PdfWriter()
+
+        # Copy over the existing document to the writer
+        writer.clone_document_from_reader(reader)
+
+        # Ensure organization is a string
+        organization_str = str(organization) if organization else ''
+
+        # Set organization metadata
+        metadata = {'/org': organization_str}
+
+        # Convert custom_metadata values to strings
+        for key, value in custom_metadata.items():
+            metadata[key] = str(value)
+
+        writer.add_metadata(metadata)
+
+        # Save the PDF with new metadata to 'sample_dummy_with_data.pdf'
+        with open("sample_dummy_with_data.pdf", 'wb') as f:
+            writer.write(f)
+
+def main():
+    random_metadata = generate_random_metadata()
+    set_pdf_metadata("dummy.pdf", "W3C", random_metadata)
+
+
+if __name__ == "__main__":
+    main()
